762f5a0c9c245499fee17fbd867928a9e888c9d1
kwonjaeil98 <kwonjaeil98@naver.com>
2019-04-11 12:53:28 +1000

    deleting old folder

--- VB > c#/Model/ISeaGrid.cs
+++
@@ -1,9 +0,0 @@
-// '' <summary>
-// '' The ISeaGrid defines the read only interface of a Grid. This
-// '' allows each player to see and attack their opponents grid.
-// '' </summary>
-public interface ISeaGrid {
-    
-    int Width {
-    }
-}
